abe8a85a9f03e0e86c00b5daa74b3eb1993ea814
rogerg6 <rogerg6@163.com>
2018-12-15 22:00:15 +0800

    algo-c

--- algo-c/sort/bubble.c
+++ algo-c/sort/bubble.c
@@ -2,9 +2,9 @@
 //2018年12月13日 16时51分
 
 //对数组排序
-for (int i = 0; i < n - 1; i++)
+for (int i = n - 1; i >= 0; i--)
 {
-	for (int j = i; j < n - 1 - i; j++)
+	for (int j = 0; j < i; j++)
 	{
 		if (a[j] > a[j + 1])    //升序
 		{
